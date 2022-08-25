4bbab9c6116306f78427a04da1d7e9d8a7617f7a
mk-hasan <mkhasan6891@gmail.com>
2022-08-25 12:39:19 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/* 
+1. DOM Selection
+2. Event Listener
+3. Basic Validation
+4. Creating Elements
+5. Appending Elements
+*/
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const year = document.querySelector('#year');
+const bookList = document.querySelector('#book-list');
